a1567cef78ee421b944903c807d7b28ae6c43216
Benjamin J Anderson <banderson@mashable.com>
2017-05-25 12:31:31 -0500

    Config local env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,21 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.middleware.insert_before 0, Rack::Cors, :debug => true, :logger => (-> { Rails.logger }) do
+    allow do
+      origins '*'
+
+      resource '/cors',
+        :headers => :any,
+        :methods => [:post],
+        :credentials => true,
+        :max_age => 0
+
+      resource '*',
+        :headers => :any,
+        :methods => [:get, :post, :delete, :put, :options, :head],
+        :max_age => 0
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'login' => 'user_sessions#create', as: :login
+  post 'logout' => 'user_sessions#destroy', as: :logout
 end
