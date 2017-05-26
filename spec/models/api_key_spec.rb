205c5332c412760610f5cca9f3ace124a4150f7c
Benjamin J Anderson <banderson@mashable.com>
2017-05-25 21:36:48 -0500

    add user specs

--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe ApiKey, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe "creating" do
+    it "sets the access_token and expires_at" do
+      key = ApiKey.create(user: user)
+      expect(key.access_token).to_not be_nil
+      expect(key.expires_at).to_not be_nil
+    end
+  end
+
+  describe ".active" do
+    let!(:active_key) { ApiKey.create(user: user, expires_at: 1.hour.from_now) }
+    let!(:inactive_key) { ApiKey.create(user: user, expires_at: 1.hour.ago) }
+
+    it "returns only active ApiKeys" do
+      expect(ApiKey.active.all).to eq [active_key]
+    end
+  end
+
+  describe ".for_user!" do
+    context "when no api key exists" do
+      it "creates an api key" do
+        expect{
+          ApiKey.for_user!(user.id)
+        }.to change { ApiKey.count }.by(1)
+      end
+    end
+
+    context "when there is an api key" do
+      context "and it is not expired" do
+        let!(:key) { ApiKey.create(user: user, expires_at: 1.hour.from_now) }
+
+        it "returns that key" do
+          expect(ApiKey.for_user!(user.id)).to eq key
+        end
+      end
+
+      context "but that key is expired" do
+        let!(:key) { ApiKey.create(user: user, expires_at: 1.hour.ago) }
+
+        it "creates an api key" do
+          api_key = ApiKey.for_user!(user.id)
+          expect(api_key.access_token).not_to eq key.access_token
+        end
+
+        it "destroys the old api_key" do
+          expect(ApiKey.where(id: key.id).count).to eq 1
+          ApiKey.for_user!(user.id)
+          expect(ApiKey.where(id: key.id).count).to eq 0
+        end
+      end
+    end
+  end
+
+  describe ".invalidate" do
+    let!(:key) { ApiKey.create(user: user, expires_at: 1.hour.from_now) }
+
+    it "sets the expires_at for the api key" do
+      expect{
+        ApiKey.invalidate(key.access_token)
+      }.to change { key.reload.expires_at }
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "password presence validation" do
+
+    let(:user) { FactoryGirl.build_stubbed(:user) }
+
+    context "when the account is a google account" do
+      context "when creating" do
+        it "does not require a password" do
+          user = User.new(email: 'auniqueemail@company.com', google_account: true)
+          expect(user).to be_valid
+        end
+      end
+
+      context "when updating other fields" do
+        it "does not require a password" do
+          user.password = nil
+          user.email = 'anotheremail@email.xyz'
+          expect(user).to be_valid
+        end
+      end
+
+      context "when updating" do
+        let(:user) { FactoryGirl.build_stubbed(:user, email: 'blah@company.com', google_account: true) }
+
+        it "does not require a password" do
+          user.password = '123'
+          expect(user).to be_valid
+        end
+      end
+    end
+  end
+
+  describe ".get_google_user" do
+    context "with a domain" do
+      let(:body) do
+        {
+          email: 'test@company.com',
+          given_name: 'test',
+          family_name: 'testing',
+          hd: 'company.com'
+        }
+      end
+
+      context "and the user already exists" do
+        context "and their attributes have not been set yet" do
+          let!(:user) { FactoryGirl.create(:user, email: 'test@company.com', first_name: 'test', last_name: 'test') }
+
+          it "updates the attributes" do
+            expect(user.first_name).to eq('test')
+            expect(user.last_name).to eq('test')
+            user = User.get_google_user(body)
+            expect(user.first_name).to_not be_nil
+            expect(user.last_name).to_not be_nil
+          end
+        end
+
+        context "and their attributes have been set" do
+          let!(:user) { FactoryGirl.create(:user, email: 'test@company.com') }
+
+          it "does nothing" do
+            first_name = user.first_name
+            last_name = user.last_name
+            user = User.get_google_user(body)
+            expect(user.first_name).to eq(first_name)
+            expect(user.last_name).to eq(last_name)
+          end
+        end
+      end
+    end
+  end
 end
